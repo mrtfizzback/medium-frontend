5620c83885cc6ea0f822da307418ffe9a5414ed1
jose_Almeida <josepedrosmith@msn.com>
2024-01-20 04:08:08 +0000

    Feed and tagFeed working

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+    apiUrl: 'https://api.realworld.io/api',
+    limit: 20,
+  }
+  
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
+import {provideHttpClient, withInterceptors} from '@angular/common/http'
+import {isDevMode} from '@angular/core'
 import {bootstrapApplication} from '@angular/platform-browser'
 import {provideRouter} from '@angular/router'
-import {AppComponent} from './app/app.component'
-import {appRoutes} from './app/app.routes'
+import {provideEffects} from '@ngrx/effects'
 import {provideState, provideStore} from '@ngrx/store'
 import {provideStoreDevtools} from '@ngrx/store-devtools'
-import {isDevMode} from '@angular/core'
+import {AppComponent} from './app/app.component'
+import {appRoutes} from './app/app.routes'
 import {authFeatureKey, authReducer} from './app/auth/store/reducers'
-import {provideEffects} from '@ngrx/effects'
 import * as authEffects from './app/auth/store/effects'
+import * as feedEffects from './app/shared/components/feed/store/effects'
+import * as popularTagsEffects from './app/shared/components/popularTags/store/effects'
 import {provideRouterStore, routerReducer} from '@ngrx/router-store'
-import {provideHttpClient, withInterceptors} from '@angular/common/http'
+import {
+  feedFeatureKey,
+  feedReducer,
+} from './app/shared/components/feed/store/reducers'
+import {
+  popularTagsFeatureKey,
+  popularTagsReducer,
+} from './app/shared/components/popularTags/store/reducers'
 import { authInterceptor } from './app/shared/services/authinterceptor'
 
 bootstrapApplication(AppComponent, {
@@ -21,7 +31,9 @@ bootstrapApplication(AppComponent, {
     }),
     provideRouterStore(),
     provideState(authFeatureKey, authReducer),
-    provideEffects(authEffects),
+    provideState(feedFeatureKey, feedReducer),
+    provideState(popularTagsFeatureKey, popularTagsReducer),
+    provideEffects(authEffects, feedEffects, popularTagsEffects),
     provideStoreDevtools({
       maxAge: 25,
       logOnly: !isDevMode(),
